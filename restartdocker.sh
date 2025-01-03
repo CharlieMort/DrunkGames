2e4f73fe5fa95afc42d3cdb7ca4e871543dc88fc
CharlieMort <charlie_mort@outlook.com>
2025-01-03 23:54:15 +0000

    stop and remove conts

--- restartdocker.sh
+++ restartdocker.sh
@@ -1,5 +1,5 @@
-docker stop drunkgames-go-web-app-1
-docker stop drunkgames-letsencrypt-nginx-proxy-companion-1
-docker stop drunkgames-nginx-proxy-1
+docker rm -f drunkgames-go-web-app-1
+docker rm -f drunkgames-letsencrypt-nginx-proxy-companion-1
+docker rm -f drunkgames-nginx-proxy-1
 docker compose -f nginx-proxy-compose.yaml up -d
 docker compose -f go-app-compose.yaml up -d
