fc53a23e77dd592a10f59e45ec67cafb153c0a93
CharlieMort <charlie_mort@outlook.com>
2025-01-03 23:59:53 +0000

    force build

--- restartdocker.sh
+++ restartdocker.sh
@@ -1,5 +1,5 @@
-docker rm -f drunkgames-go-web-app-1
-docker rm -f drunkgames-letsencrypt-nginx-proxy-companion-1
-docker rm -f drunkgames-nginx-proxy-1
-docker compose -f nginx-proxy-compose.yaml up -d
-docker compose -f go-app-compose.yaml up -d
+docker compose down drunkgames-go-web-app-1
+docker compose down drunkgames-letsencrypt-nginx-proxy-companion-1
+docker compose down drunkgames-nginx-proxy-1
+docker compose -f nginx-proxy-compose.yaml up -d --build
+docker compose -f go-app-compose.yaml up -d --build
