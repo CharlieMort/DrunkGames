f9074f47523972176349bfc7d0d84f05f1fa851c
CharlieMort <charlie_mort@outlook.com>
2025-01-04 00:10:30 +0000

    better restart

--- restartdocker.sh
+++ restartdocker.sh
@@ -1,6 +1,5 @@
-docker compose down drunkgames-go-web-app-1
-docker compose down drunkgames-letsencrypt-nginx-proxy-companion-1
-docker compose down drunkgames-nginx-proxy-1
+docker compose -f nginx-proxy-compose down
+docker compose -f go-app-compose.yaml down
 docker compose -f nginx-proxy-compose.yaml build --no-cache
 docker compose -f go-app-compose.yaml build --no-cache
 docker compose -f nginx-proxy-compose.yaml up -d
