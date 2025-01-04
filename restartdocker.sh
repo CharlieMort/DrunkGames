f4c78aa8315348c9bb73ca6fe0a0f96d0a0f1399
CharlieMort <charlie_mort@outlook.com>
2025-01-04 00:39:06 +0000

    dont put down nginx proxy

--- restartdocker.sh
+++ restartdocker.sh
@@ -1,6 +1,3 @@
-docker compose -f nginx-proxy-compose.yaml down
 docker compose -f go-app-compose.yaml down
-docker compose -f nginx-proxy-compose.yaml build --no-cache
 docker compose -f go-app-compose.yaml build --no-cache
-docker compose -f nginx-proxy-compose.yaml up -d
 docker compose -f go-app-compose.yaml up -d
