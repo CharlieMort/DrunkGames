a1f9912be836d4f780229d15298de0acba7b2d57
CharlieMort <charlie_mort@outlook.com>
2025-01-04 00:14:51 +0000

    shit

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module github.com/CharlieMort/DrunkGamesServer
 
 go 1.23.0
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/websocket v1.5.3
+)
--- server/main.go
+++ server/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"math/rand"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 const PORT = ":80"
@@ -31,22 +32,40 @@ var nextClientID int
 func main() {
 	fmt.Println("Drunk Games Server Running...")
 
-	nextClientID = 1
-	hub := NewHub()
-	go hub.run()
+	// nextClientID = 1
+	// hub := NewHub()
+	// go hub.run()
 
-	buildHandler := http.FileServer(http.Dir("./build"))
-	http.Handle("/", buildHandler)
+	// buildHandler := http.FileServer(http.Dir("./build"))
+	// http.Handle("/", buildHandler)
 
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		enableCors(&w)
-		log.Println("Someone Connected to ws")
-		serveWs(hub, w, r)
+	// http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	// 	enableCors(&w)
+	// 	log.Println("Someone Connected to ws")
+	// 	serveWs(hub, w, r)
+	// })
+
+	// fmt.Printf("Listening On Port: %s\n", PORT)
+	// err := http.ListenAndServe(PORT, nil)
+	// if err != nil {
+	// 	log.Fatal("ListenAndServe: ", err)
+	// }
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>This is the homepage. Try /hello and /hello/Sammy\n</h1>")
 	})
 
-	fmt.Printf("Listening On Port: %s\n", PORT)
-	err := http.ListenAndServe(PORT, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>Hello from Docker!\n</h1>")
+	})
+
+	r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["name"]
+
+		fmt.Fprintf(w, "<h1>Hello, %s!\n</h1>", title)
+	})
+
+	http.ListenAndServe(":80", r)
 }
