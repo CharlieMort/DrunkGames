71d26353b4b6fa7508c8835a8b84bd661484f539
CharlieMort <charlie_mort@outlook.com>
2025-01-05 12:03:54 +0000

    devug

--- server/main.go
+++ server/main.go
@@ -97,6 +97,7 @@ type spaHandler struct {
 // file located at the index path on the SPA handler will be served. This
 // is suitable behavior for serving an SPA (single page application).
 func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("DOING SHIT")
 	// Join internally call path.Clean to prevent directory traversal
 	path := filepath.Join(h.staticPath, r.URL.Path)
 
